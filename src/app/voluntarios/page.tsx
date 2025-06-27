dd6596bcb2a0c7965cd14520b1f591214396e2c8
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2025-06-26 22:03:07 -0300

    feat: listar voluntários na página principal com fetch da API e exibição em grid

--- src/app/voluntarios/page.tsx
+++ src/app/voluntarios/page.tsx
@@ -1,13 +1,15 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
 import DialogNovoVoluntario from "./novo/page";
 import { toast } from "sonner";
+import CardIgreja from "@/components/Cards/CardVoluntario";
 
 type Voluntario = {
   id: number;
   nome: string;
-  funcao: string,
+  funcao: string;
+  igrejaId: string;
 };
 
 export default function Page() {
@@ -22,11 +24,21 @@ export default function Page() {
       });
   }, []);
   return (
-    <div>
-      {voluntarios.map((voluntario) => (
-        <div key={voluntario.id}>{voluntario.nome} {voluntario.funcao}</div>
-      ))}
-      <DialogNovoVoluntario />
+    <div className="p-3">
+      <div className="flex justify-end mb-3">
+        <DialogNovoVoluntario />
+      </div>
+      <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {Array.isArray(voluntarios) &&
+          voluntarios.map((voluntario) => (
+            <CardIgreja
+              key={voluntario.id}
+              nome={voluntario.nome}
+              funcao={voluntario.funcao}
+              igrejaId={voluntario.igrejaId}
+            />
+          ))}
+      </div>
     </div>
   );
 }
