6a12f21704a361c2e3b4d81d3df66e4955346692
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2025-07-02 00:56:06 -0300

    feat: input para busca por nome

--- src/app/igrejas/page.tsx
+++ src/app/igrejas/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import DialogNovaIgreja from "./nova/page";
 import CardIgreja from "@/components/Cards/CardIgreja";
+import { Input } from "@/components/ui/input";
 
 type Igreja = {
   id: number;
@@ -14,24 +15,43 @@ type Igreja = {
 
 export default function Page() {
   const [igrejas, setIgrejas] = useState<Igreja[]>([]);
+  const [busca, setBusca] = useState("");
+  const [filtrados, setFiltrados] = useState<Igreja[]>([]);
 
   useEffect(() => {
     fetch("/api/igrejas")
       .then((res) => res.json())
-      .then((data) => setIgrejas(data))
+      .then((data) => {
+        setIgrejas(data);
+        setFiltrados(data);
+      })
       .catch((err) => {
-        toast.error("Erro ao buscar igrejas", err);
+        toast.error("Erro ao Buscar Igrejas", err);
       });
   }, []);
+
+  useEffect(() => {
+    const buscaNormalizada = busca.toLowerCase();
+    const resultado = igrejas.filter((i) =>
+      i.nome.toLowerCase().includes(buscaNormalizada)
+    );
+    setFiltrados(resultado);
+  }, [busca, igrejas]);
   return (
     <div className="p-3">
-      <div className="flex justify-end mb-3">
+      <div className="flex justify-between items-center gap-2 mb-3">
+        <Input
+          type="text"
+          placeholder="Buscar por nome..."
+          value={busca}
+          onChange={(e) => setBusca(e.target.value)}
+          className="w-full max-w-md"
+        />
         <DialogNovaIgreja />
       </div>
 
       <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {Array.isArray(igrejas) &&
-        igrejas.map((igreja) => (
+        {filtrados.map((igreja) => (
           <CardIgreja
             key={igreja.id}
             nome={igreja.nome}
--- src/app/voluntarios/page.tsx
+++ src/app/voluntarios/page.tsx
@@ -3,11 +3,13 @@
 import { useEffect, useState } from "react";
 import DialogNovoVoluntario from "./novo/page";
 import { toast } from "sonner";
-import CardIgreja from "@/components/Cards/CardVoluntario";
+import { Input } from "@/components/ui/input";
+import CardVoluntario from "@/components/Cards/CardVoluntario";
 
 type Voluntario = {
   id: number;
   nome: string;
+  comum: string;
   funcao: string;
   igrejaId: string;
   foto?: string;
@@ -15,31 +17,54 @@ type Voluntario = {
 
 export default function Page() {
   const [voluntarios, setVoluntarios] = useState<Voluntario[]>([]);
+  const [busca, setBusca] = useState("");
+  const [filtrados, setFiltrados] = useState<Voluntario[]>([]);
 
   useEffect(() => {
     fetch("/api/voluntarios")
       .then((res) => res.json())
-      .then((data) => setVoluntarios(data))
+      .then((data) => {
+        setVoluntarios(data);
+        setFiltrados(data);
+      })
       .catch((err) => {
         toast.error("Erro ao buscar Voluntários", err);
       });
   }, []);
+
+  useEffect(() => {
+    const buscaNormalizada = busca.toLowerCase();
+    const resultado = voluntarios.filter(
+      (v) =>
+        v.nome.toLowerCase().includes(buscaNormalizada) ||
+        v.comum.toLowerCase().includes(buscaNormalizada)
+    );
+    setFiltrados(resultado);
+  }, [busca, voluntarios]);
+
   return (
     <div className="p-3">
-      <div className="flex justify-end mb-3">
+      <div className="flex justify-between items-center gap-2 mb-3">
+        <Input
+          type="text"
+          placeholder="Buscar por nome ou comum..."
+          value={busca}
+          onChange={(e) => setBusca(e.target.value)}
+          className="w-full max-w-md"
+        />
         <DialogNovoVoluntario />
       </div>
+
       <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {Array.isArray(voluntarios) &&
-          voluntarios.map((voluntario) => (
-            <CardIgreja
-              key={voluntario.id}
-              nome={voluntario.nome}
-              funcao={voluntario.funcao}
-              igrejaId={voluntario.igrejaId}
-              foto={voluntario.foto}
-            />
-          ))}
+        {filtrados.map((voluntario) => (
+          <CardVoluntario
+            key={voluntario.id}
+            nome={voluntario.nome}
+            funcao={voluntario.funcao}
+            igrejaId={voluntario.igrejaId}
+            foto={voluntario.foto}
+          />
+        ))}
       </div>
     </div>
   );
